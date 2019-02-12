e14e74502a2c3e33e8bdc52576a2723a2aa0bc61
Toshiaki Aoike <aoike@cloudpack.jp>
2019-02-12 10:37:05 +0900

    modify setting

--- cfn/_remove.sh
+++ cfn/_remove.sh
@@ -10,15 +10,17 @@ aws s3 rm s3://$SYS_NAME --recursive
 echo "delete code repository"
 aws codecommit delete-repository --repository-name $SYS_NAME
 
-echo "delete stack"
-aws cloudformation delete-stack --stack-name $SYS_NAME
-echo "stack deleting ..."
-aws cloudformation wait stack-delete-complete --stack-name $SYS_NAME
-
 echo "delete deploy stack"
 aws cloudformation delete-stack --stack-name $SYS_NAME-deploy
 echo "deploy stack deleting ..."
 aws cloudformation wait stack-delete-complete --stack-name $SYS_NAME-deploy
+
+sleep 1
+
+echo "delete stack"
+aws cloudformation delete-stack --stack-name $SYS_NAME
+echo "stack deleting ..."
+aws cloudformation wait stack-delete-complete --stack-name $SYS_NAME
 
 echo "remove finished !"
 
--- cfn/_variable.sh
+++ cfn/_variable.sh
@@ -1,6 +1,6 @@
 # variable
 export STAGE="dev"
-export NAME="cicd-l-a"
+export NAME="cicd-l-sa"
 export SYS_NAME=$NAME-$STAGE
 export BRANCH="master"
 export AWS_DEFAULT_REGION="ap-northeast-1"
